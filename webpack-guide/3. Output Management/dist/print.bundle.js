ff541b05a591e5091d7dc6fe1ba81b9aaaaee632
phaddad <p.haddad.p@gmail.com>
2017-12-23 20:04:24 -0300

    Installation,Getting Started,Asset Management,Output Management,Development

--- webpack-guide/3. Output Management/dist/print.bundle.js
+++ webpack-guide/3. Output Management/dist/print.bundle.js
@@ -1,14 +1,4 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("lodash"));
-	else if(typeof define === 'function' && define.amd)
-		define(["lodash"], factory);
-	else if(typeof exports === 'object')
-		exports["webpackNumbers"] = factory(require("lodash"));
-	else
-		root["webpackNumbers"] = factory(root["_"]);
-})(typeof self !== 'undefined' ? self : this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
-return /******/ (function(modules) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -79,42 +69,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony export (immutable) */ __webpack_exports__["numToWord"] = numToWord;
-/* harmony export (immutable) */ __webpack_exports__["wordToNum"] = wordToNum;
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ref_json__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ref_json___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__ref_json__);
+/* harmony export (immutable) */ __webpack_exports__["default"] = printMe;
+function printMe() {
+  console.log('I get called from print.js!');
+}
 
-
-
-console.log("numRef", __WEBPACK_IMPORTED_MODULE_1__ref_json___default.a);
-console.log("undescore -> ", __WEBPACK_IMPORTED_MODULE_0_lodash___default.a);
-
-function numToWord(num) {
-  return __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.reduce(__WEBPACK_IMPORTED_MODULE_1__ref_json___default.a, (accum, ref) => {
-    return ref.num === num ? ref.word : accum;
-  }, '');
-};
-
-function wordToNum(word) {
-  return __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.reduce(__WEBPACK_IMPORTED_MODULE_1__ref_json___default.a, (accum, ref) => {
-    return ref.word === word && word.toLowerCase() ? ref.num : accum;
-  }, -1);
-};
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-module.exports = [{"num":1,"word":"One"},{"num":2,"word":"Two"},{"num":3,"word":"Three"},{"num":4,"word":"Four"},{"num":5,"word":"Five"},{"num":0,"word":"Zero"}]
 
 /***/ })
-/******/ ])["default"];
-});
+/******/ ]);
---
+++ webpack-guide/3. Output Management/run.bat
@@ -0,0 +1,4 @@
+REM webpack src/index.js dist/bundle.js
+REM webpack --config webpack.config.js
+
+npm run build
